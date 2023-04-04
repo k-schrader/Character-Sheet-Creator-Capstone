38e7528e72261c86c10738c276998a698e380a42
Kalie Schrader <kalie.j.schrader@gmail.com>
2023-04-04 12:18:20 -0400

    Fixed url issue

--- src/main/java/org/kalieschrader/CSCPractice2/repository/CharacterRaceRepository.java
+++ src/main/java/org/kalieschrader/CSCPractice2/repository/CharacterRaceRepository.java
@@ -1,13 +1,9 @@
 package org.kalieschrader.CSCPractice2.repository;
-import java.util.List;
 import java.util.Optional;
 
 import org.kalieschrader.CSCPractice2.model.CharacterRace;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
-@Repository
 public interface CharacterRaceRepository extends JpaRepository<CharacterRace, Long>{
 
 	Optional<CharacterRace> findByRaceName(String raceName);
